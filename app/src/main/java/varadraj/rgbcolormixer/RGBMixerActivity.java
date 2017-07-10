e2ce0425fc12caccf2120cba89afeaf99b95e581
varad <vrajvaradraj@gmail.com>
2017-07-10 10:18:15 +0530

    Working on Activity Layout. TextView and SurfaceView added

---
+++ app/src/main/java/varadraj/rgbcolormixer/RGBMixerActivity.java
@@ -0,0 +1,13 @@
+package varadraj.rgbcolormixer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RGBMixerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rgbmixer);
+    }
+}
